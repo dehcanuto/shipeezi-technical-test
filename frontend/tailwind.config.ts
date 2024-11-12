79f2db977841aef04c95bc3011c985adf40593ae
André Canuto <andrecanutoc@gmail.com>
2024-11-12 12:06:35 -0300

    feat: insert color variables

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -9,6 +9,21 @@ export default {
   theme: {
     extend: {
       colors: {
+        green: {
+          '8%':  '#449c47',
+          '12%': '#449c47',
+          '16%': '#449c47',
+          '50':  '#ecf5ed',
+          '100': '#c5e0c6',
+          '200': '#a9d1aa',
+          '300': '#82bd84',
+          '400': '#69b06c',
+          '500': '#449c47',
+          '600': '#3e8e41',
+          '700': '#306f32',
+          '800': '#255627',
+          '900': '#1d421e'
+        },
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
