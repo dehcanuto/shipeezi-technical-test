14e4f2c60fd626259100adfea5de516d79f56957
André Canuto <andrecanutoc@gmail.com>
2024-11-18 11:04:46 -0300

    feat: component in register user

--- shipeezi-frontend/src/pages/gerencial/users/NewUser.tsx
+++ shipeezi-frontend/src/pages/gerencial/users/NewUser.tsx
@@ -1,11 +1,9 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-import { BaseButton, Breadcrumbs, FormField, FormFieldSelect } from "../../../components";
+import { Breadcrumbs, FormUser } from "../../../components";
 import GerencialLayout from "../../../components/organisms/GerencialLayout";
-import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
-import { genderTypes } from "../../../enums";
-import addressTypes from "../../../enums/address.type";
+import { FieldValues, SubmitHandler } from "react-hook-form";
 import { handleCreateUser } from "../../../hooks/users";
 
 const NewUserBreadcumb = [
@@ -22,7 +20,6 @@ const NewUserBreadcumb = [
 function NewUserPage() {
   const navigate = useNavigate();
   const [loading, setLoading] = useState<boolean>(false);
-  const { register, handleSubmit } = useForm<FieldValues>();
     
   const onSubmit: SubmitHandler<FieldValues> = async (data) => {
     setLoading(true);
@@ -41,78 +38,10 @@ function NewUserPage() {
       <div className="border-b">
         <Breadcrumbs items={NewUserBreadcumb} />
       </div>
-      <form onSubmit={handleSubmit(onSubmit)} className="divide-y">
-        <div className="grid grid-cols-3 py-8 gap-3">
-          <div>
-            <h3 className="font-semibold">Personal details</h3>
-            <p className="text-slate-500">Information about the person, such as name, contact details, address, and other relevant personal information.</p>
-          </div>
-          <div className="col-span-2 grid grid-cols-2 gap-3">
-            <FormField
-              label="Full name"
-              name="fullName"
-              placeholder="Please enter the full name"
-              register={register} />
-            <FormField
-              label="Username"
-              name="username"
-              placeholder="@"
-              register={register} />
-            <FormField
-              label="Email"
-              name="email"
-              placeholder="Please enter the email"
-              register={register} />
-            <FormField
-              label="Password"
-              name="password"
-              type="password"
-              placeholder="Enter your password"
-              register={register} />
-            <FormField
-              label="Birth date"
-              name="birthdate"
-              placeholder="dd/mm/yyyy"
-              register={register} />
-            <FormFieldSelect
-              label="Gender"
-              name="gender"
-              options={genderTypes}
-              register={register} />
-            <FormField
-              label="Phone number"
-              name="phone"
-              placeholder="000 000 0000"
-              register={register} />
-          </div>
-        </div>
-        <div className="grid grid-cols-3 py-8 gap-3">
-          <div>
-            <h3 className="font-semibold">Addresses</h3>
-            <p className="text-slate-500">All addresses that are linked to this person.</p>
-          </div>
-          <div className="col-span-2 grid grid-cols-3 gap-3">
-            <div className="col-span-2">
-              <FormField
-                label="Address"
-                name="address"
-                placeholder="Enter the address here"
-                register={register} />
-            </div>
-            <FormFieldSelect
-              label="Address type"
-              name="type"
-              options={addressTypes}
-              register={register} />
-          </div>
-        </div>
-        <div className="flex py-4 items-center justify-end border-t">
-          <div className="flex gap-3">
-              <BaseButton label="Cancel" variant="text" click={() => null} />
-              <BaseButton label="Create" type="submit" loading={loading} />
-          </div>
-        </div>
-      </form>
+      <FormUser
+        onSubmit={onSubmit}
+        loading={loading}
+      />
     </GerencialLayout>
   );
 };
