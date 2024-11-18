9bd141771faae89c7b3886c80efb3acdb4158374
André Canuto <andrecanutoc@gmail.com>
2024-11-18 11:19:49 -0300

    feat: adjust tag render

--- shipeezi-frontend/src/misc/renderTags.tsx
+++ shipeezi-frontend/src/misc/renderTags.tsx
@@ -2,10 +2,20 @@ import { Key } from "react";
 import { tagsTasks } from "../enums";
 import { TagsTasksPropTypes } from "../enums/tags.tasks";
 
-export const RenderTags = ({ tags }: { tags: string }) => {
-    const tagsIds = JSON.parse(tags).map((tagId: number) =>
-        tagsTasks.find((tag) => tag.value === tagId)
-    );
+export const RenderTags = ({ tags }: { tags: string | number[] }) => {
+    let tagsFormmated;
+    let tagsIds: TagsTasksPropTypes[] = [];
+
+    try {
+        tagsFormmated = JSON.parse(tags as string)
+    } catch {
+        tagsFormmated = tags;
+    }
+    finally {
+        tagsIds = tagsFormmated.map((tagId: number) =>
+            tagsTasks.find((tag) => tag.value === tagId)
+        );
+    }
 
     return (
         <div className="flex gap-1">
