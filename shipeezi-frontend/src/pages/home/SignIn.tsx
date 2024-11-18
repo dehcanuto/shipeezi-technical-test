dd4949aea35ba30a129ff425182363b4104d4095
André Canuto <andrecanutoc@gmail.com>
2024-11-18 18:29:44 -0300

    feat: logo at homepages

--- shipeezi-frontend/src/pages/home/SignIn.tsx
+++ shipeezi-frontend/src/pages/home/SignIn.tsx
@@ -1,14 +1,20 @@
+import { BaseLogo } from "../../components";
 import SignIn from "../../components/organisms/SignIn";
 
 function SignInPage() {
   return (
-    <div className="w-full min-h-screen bg-white flex flex-col sm:justify-center items-center pt-6 sm:pt-0">
-      <div className="w-full sm:max-w-md p-5 mx-auto">
-        <div className="space-y-3 mb-6">
-          <h2 className="text-center text-5xl font-extrabold">Welcome back!</h2>
-          <p>Sign in to access your dashboard and company details.</p>
+    <div>
+      <nav className="absolute top-0 flex items-center py-4 px-8">
+        <BaseLogo />
+      </nav>
+      <div className="w-full min-h-screen bg-white flex flex-col sm:justify-center items-center pt-6 sm:pt-0">
+        <div className="w-full sm:max-w-md p-5 mx-auto">
+          <div className="space-y-3 mb-6">
+            <h2 className="text-center text-5xl font-extrabold">Welcome back!</h2>
+            <p>Sign in to access your dashboard and company details.</p>
+          </div>
+          <SignIn />
         </div>
-        <SignIn />
       </div>
     </div>
   );
--- shipeezi-frontend/src/pages/home/SignUp.tsx
+++ shipeezi-frontend/src/pages/home/SignUp.tsx
@@ -1,14 +1,20 @@
+import { BaseLogo } from "../../components";
 import SignUp from "../../components/organisms/SignUp";
 
 function SignUpPage() {
   return (
-    <div className="w-full min-h-screen bg-white flex flex-col sm:justify-center items-center pt-6 sm:pt-0">
-      <div className="w-full sm:max-w-lg p-5 mx-auto">
-        <div className="space-y-3 text-center mb-6">
-          <h2 className="text-5xl font-extrabold">Create Your Account</h2>
-          <p>Welcome to CompanyHub! Sign up now to unlock exclusive content and features.</p>
+    <div>
+      <nav className="absolute top-0 flex items-center py-4 px-8">
+        <BaseLogo />
+      </nav>
+      <div className="w-full min-h-screen bg-white flex flex-col sm:justify-center items-center pt-6 sm:pt-0">
+        <div className="w-full sm:max-w-lg p-5 mx-auto">
+          <div className="space-y-3 text-center mb-6">
+            <h2 className="text-5xl font-extrabold">Create Your Account</h2>
+            <p>Welcome to CompanyHub! Sign up now to unlock exclusive content and features.</p>
+          </div>
+          <SignUp />
         </div>
-        <SignUp />
       </div>
     </div>
   );
